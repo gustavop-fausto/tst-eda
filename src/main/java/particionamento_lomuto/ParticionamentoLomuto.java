4b352914fd2c11b11d751424e59a202bb1ed0917
gustavop-fausto <gugapf08@gmail.com>
2026-05-18 23:16:40 -0300

    add: particionamento lomuto

---
+++ src/main/java/particionamento_lomuto/ParticionamentoLomuto.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ParticionamentoLomuto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = Arrays.stream(sc.nextLine().split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+
+        quickSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void quickSort(int[] array, int ini, int fim) {
+        int idx = partition(array, ini, fim);
+    }
+
+    private static int partition(int[] array, int ini, int fim) {
+        int j = fim;
+
+        for (int i = fim - 1; i >= ini; i--) {
+            if (array[i] > array[fim]) {
+                j--;
+                swap(array, i, j);
+                System.out.println(Arrays.toString(array));
+            }
+        }
+
+        swap(array, j, fim);
+        System.out.println(Arrays.toString(array));
+
+        return j;
+    } 
+
+    private static void swap(int[] array, int i, int j) {
+        int aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
+    }
+}
